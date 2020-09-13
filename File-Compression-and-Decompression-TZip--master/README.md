635544b81b6b9b2ed28fc60204df2ee28177e629
Shreya Monpara <20968224+shreyamonpara@users.noreply.github.com>
2020-09-13 18:05:17 -0400

    Add files via upload

---
+++ File-Compression-and-Decompression-TZip--master/README.md
@@ -0,0 +1,3 @@
+# File-Compression-and-Decompression-TZip
+Technology Used: Java, Swing
+A Desktop based File Compression and Decompression was developed, inspired by 7-zip. It supports compression and decompression of text files using Huffman, LZW, and RLE algorithms.
---
+++ File-Compression-and-Decompression-TZip--master/src/tzipgui/TZipGUI.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tzipgui;
+
+import myTZip.TZip;
+
+/**
+ *
+ * @author Shreya
+ */
+public class TZipGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	// TODO code application logic here
+	TZip frame =new TZip();
+	frame.setVisible(true);
+    }
+    
+}
